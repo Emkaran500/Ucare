00ae8867caef388c56572dce0bcd6534a6df24f5
Emil B <emkaran_500@mail.ru>
2024-07-11 18:01:10 +0400

    Add CRUD to Place Views

--- UcareApp/src/UcareApp.Presentation/Controllers/PlaceController.cs
+++ UcareApp/src/UcareApp.Presentation/Controllers/PlaceController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using UcareApp.Core.Place.Services;
 using UcareApp.Core.Place.Base;
+using UcareApp.Core.Place.Models;
 
 namespace UcareApp.Presentation.Controllers;
 
@@ -19,7 +20,46 @@ public class PlaceController : Controller
     [AllowAnonymous]
     public async Task<IActionResult> Index()
     {
-        var places = await this.placeService.GetAllAsync();
+        var places = await this.placeService.GetAllPlacesAsync();
         return View(places);
+    }
+
+    [ActionName("Get")]
+    [HttpGet("[controller]/[action]/{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var place = await placeService.GetPlaceByIdAsync(id);
+
+        return base.View("Index", place);
+    }
+
+    [HttpGet("[action]", Name = "CreatePlace")]
+    public IActionResult Create()
+    {
+        return base.View();
+    }
+
+    [HttpPost("[controller]/[action]")]
+    public async Task<IActionResult> Create(Place newPlace)
+    {
+        await placeService.CreateNewPlaceAsync(newPlace);
+
+        return base.View("Index");
+    }
+
+    [HttpGet("[action]/{id}", Name = "UpdatePlace")]
+    public async Task<IActionResult> Update(Guid id)
+    {
+        var place = await this.placeService.GetPlaceByIdAsync(id);
+
+        return base.View(place);
+    }
+
+    [HttpGet("api/[controller]/[action]")]
+    public async Task<IActionResult> Update(Place? place)
+    {
+        await this.placeService.UpdatePlaceAsync(place);
+
+        return base.RedirectToAction("Get", new {place?.Id});
     }
 }
