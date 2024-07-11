f25eabfb7a2aba3980700205031751cc562846e0
Emil B <emkaran_500@mail.ru>
2024-07-11 14:02:35 +0400

    Add Update, Create, Delete to Places Srvc

--- UcareApp/src/UcareApp.Core/Place/Services/PlaceService.cs
+++ UcareApp/src/UcareApp.Core/Place/Services/PlaceService.cs
@@ -22,4 +22,25 @@ public class PlaceService :  IPlaceService
         
         return await this.placeRepository.GetByIdAsync(id.Value);
     }
+
+    public async Task CreateNewPlaceAsync(IPlace? newPlace)
+    {
+        ArgumentNullException.ThrowIfNull(newPlace);
+
+        await this.placeRepository.CreateAsync(newPlace);
+    }
+
+    public async Task DeletePlaceAsync(int? id)
+    {
+        ArgumentNullException.ThrowIfNull(id);
+
+        await this.placeRepository.DeleteAsync(id);
+    }
+
+    public async Task UpdatePlaceAsync(IPlace? place)
+    {
+        ArgumentNullException.ThrowIfNull(place);
+
+        await this.placeRepository.UpdateAsync(place);
+    }
 }
