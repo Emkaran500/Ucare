6d943ca685a17ffd0f6e273fad464045347512a0
Emil B <emkaran_500@mail.ru>
2024-07-11 13:57:54 +0400

    Add Update, Create and Delete to Places Repo

--- UcareApp/src/UcareApp.Core/Place/Repositories/PlaceDapperRepository.cs
+++ UcareApp/src/UcareApp.Core/Place/Repositories/PlaceDapperRepository.cs
@@ -20,4 +20,50 @@ public class PlaceDapperRepository : IPlaceRepository
 
         return await connection.QueryAsync<IPlace>($"Select * from Places Where Places.Id = {id}");
     }
+
+    public async Task CreateAsync(IPlace? newPlace)
+    {
+        using var connection = new NpgsqlConnection(this.connectionString);
+
+        await connection.ExecuteAsync($@"Insert into Places({nameof(newPlace.Name)},
+                                                            {nameof(newPlace.Adress)},
+                                                            {nameof(newPlace.Longitude)},
+                                                            {nameof(newPlace.Latitude)},
+                                                            {nameof(newPlace.WorkingDays)},
+                                                            {nameof(newPlace.Maintenances)}) 
+                                         Values ({newPlace.Name},
+                                                 {newPlace.Adress},
+                                                 {newPlace.Longitude},
+                                                 {newPlace.Latitude},
+                                                 ARRAY{newPlace.WorkingDays},
+                                                 ARRAY{newPlace.Maintenances})");
+    }
+
+    public async Task DeleteAsync(Guid? id)
+    {
+        using var connection = new NpgsqlConnection(this.connectionString);
+
+        var placesIds = await connection.QueryAsync<int>("Select Id From Places");
+
+        var containsId = placesIds.Contains(id.Value);
+
+        if (containsId)
+        {
+            await connection.ExecuteAsync($"Delete from Places Where Places.Id = {id}");
+        }
+    }
+
+    public async Task<long> UpdateAsync(IPlace? place)
+    {
+        using var connection = new NpgsqlConnection(this.connectionString);
+
+        return await connection.ExecuteAsync($@"Update Places
+                                                Set {nameof(place.Name)} = {place.Name},
+                                                    {nameof(newPlace.Adress)} = {newPlace.Adress},
+                                                    {nameof(newPlace.Longitude)} = {newPlace.Longitude},
+                                                    {nameof(newPlace.Latitude)} = {newPlace.Latitude},
+                                                    {nameof(newPlace.WorkingDays)} = ARRAY{newPlace.WorkingDays},
+                                                    {nameof(newPlace.Maintenances)} = ARRAY{newPlace.Maintenances}
+                                                Where Places.Id = {place.Id}");
+    }
 }
